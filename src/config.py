8c5784eecc9db741173b01f1860c7862974d83c2
KhairullovR <Ruslan.Khairullov@dlubal.com>
2024-09-03 12:39:20 +0200

    changing the .env file

--- src/config.py
+++ src/config.py
@@ -1,17 +1,57 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
     DB_SERVER: str
     DB_NAME: str
     DB_DRIVER: str
+    INITIAL_CATALOG: str
+    TRUSTED_CONNECTION: str
 
     @property
     def DATABASE_URL_pyodbc(self):
-        # mssql+pyodbc://CZ-DBS1.dlubal.local/netgenium?driver=SQL+Server
-        return f"mssql+pyodbc://{self.DB_SERVER}/{self.DB_NAME}?driver={self.DB_DRIVER}"
+        # Constructing the connection string with Initial Catalog and Trusted_Connection
+        return (
+            f"mssql+pyodbc://{self.DB_SERVER}/{self.INITIAL_CATALOG}?"
+            f"driver={self.DB_DRIVER}&"
+            f"Trusted_Connection={self.TRUSTED_CONNECTION}"
+        )
 
-    
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+# class Settings(BaseSettings):
+#     DB_SERVER: str
+#     DB_NAME: str
+#     DB_DRIVER: str
+
+#     @property
+#     def DATABASE_URL_pyodbc(self):
+#         # mssql+pyodbc://CZ-DBS1.dlubal.local/netgenium?driver=SQL+Server
+#         return f"mssql+pyodbc://{self.DB_SERVER}/{self.DB_NAME}?driver={self.DB_DRIVER}"
+
+    
+#     model_config = SettingsConfigDict(env_file=".env")
+
+# settings = Settings()
